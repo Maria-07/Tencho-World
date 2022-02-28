de37fa5e1055ac95476d85b83201db4f1ce51f77
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-02-28 23:39:49 +0600

    error message show and search box test successful

--- js/app.js
+++ js/app.js
@@ -1,9 +1,20 @@
-// search box 
+// error msg
+const errorMessage = () => {
+    document.getElementById('error-message').style.display = 'block';
+}
 
+// search box 
 const searchPhone = () => {
     const searchBox = document.getElementById('search-box');
     const searchText = searchBox.value;
     console.log(searchText);
+    searchBox.value = '';
 
-    searchBox.value='';
+    if (searchText == '') {
+        errorMessage();
+    } else {
+        document.getElementById('error-message').style.display = 'none';
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        console.log(url);
+    }
 }
