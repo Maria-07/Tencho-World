1df8064f1d61b45853a3c668a9a64e8017126e1c
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2022-03-01 00:34:12 +0600

    display data insert

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-// error msg
-const errorMessage = () => {
-    document.getElementById('error-message').style.display = 'block';
-}
-
 // search box 
 const searchPhone = () => {
     const searchBox = document.getElementById('search-box');
@@ -15,6 +10,38 @@ const searchPhone = () => {
     } else {
         document.getElementById('error-message').style.display = 'none';
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
-        console.log(url);
+        // console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchPhone(data.data));
     }
+}
+
+// display data 
+const displaySearchPhone = phoneData => {
+    const searchPhone = document.getElementById('search-phone');
+    phoneData.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                    <img src="${phone.image}" class="card-img-top" alt="phone Image">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+        `;
+        searchPhone.appendChild(div);
+    })
+}
+
+// error msg
+const errorMessage = () => {
+    document.getElementById('error-message').style.display = 'block';
 }
